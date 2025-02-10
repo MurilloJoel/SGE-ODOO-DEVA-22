3cfb1bcd3df7a1ba33aa84fb99270e68db234f97
MurilloJoel <joelolbejo187@gmail.com>
2025-02-10 09:44:30 +0100

    Incluimos el modelo Categoria

---
+++ odoo/addons/libreria/models/Categoria.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class Categoria(models.model):
+    _name = 'libreria2.categoria'
+    _description = 'Categoría'
+    
+    name = fields.Char(string='Nombre',required=True,help='Introduce el nombre de la categoria')
+    description = fields.Text(string='Descripcion',help='Introduce una descripcion')
