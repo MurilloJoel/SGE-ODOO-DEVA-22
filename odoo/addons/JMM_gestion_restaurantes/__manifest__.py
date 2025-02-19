1b66e70a9e3e8408f8e042bfdf91a76ccb0503b8
MurilloJoel <joelolbejo187@gmail.com>
2025-02-19 13:37:36 +0100

    Vistas planteadas

--- odoo/addons/JMM_gestion_restaurantes/__manifest__.py
+++ odoo/addons/JMM_gestion_restaurantes/__manifest__.py
@@ -23,7 +23,8 @@ Long description of module's purpose
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/dish_views.xml'
+        'views/dish_views.xml',
+        'views/category_views.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
